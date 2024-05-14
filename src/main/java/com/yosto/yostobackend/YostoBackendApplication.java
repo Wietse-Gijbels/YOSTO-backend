33e0c3bc889123cce12a75853421b3621b9db9af
kunovercammen <kuno.vercammen@student.ucll.be>
2024-05-14 09:39:50 +0200

    Initial setup

---
+++ src/main/java/com/yosto/yostobackend/YostoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yosto.yostobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YostoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YostoBackendApplication.class, args);
+    }
+
+}
