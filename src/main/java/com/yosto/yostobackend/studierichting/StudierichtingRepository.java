ed2f7f66b61f993f9c4019ea689d28e2b80524d7
stan435 <stan.baeckelandt@telenet.be>
2024-05-21 16:12:15 +0200

    studierichting en import sql data

---
+++ src/main/java/com/yosto/yostobackend/studierichting/StudierichtingRepository.java
@@ -0,0 +1,10 @@
+package com.yosto.yostobackend.studierichting;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface StudierichtingRepository extends JpaRepository<Studierichting, UUID> {
+
+
+}
