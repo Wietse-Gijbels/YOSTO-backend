e577737f68397a1c575d03a0d5c82af133452a40
Wietse-Gijbels <wietse.gijbels@student.ucll.be>
2024-06-05 15:44:19 +0200

    working, add switch button and function

--- src/main/java/com/yosto/yostobackend/auth/AuthenticationResponseBuilder.java
+++ src/main/java/com/yosto/yostobackend/auth/AuthenticationResponseBuilder.java
@@ -1,20 +1,29 @@
 package com.yosto.yostobackend.auth;
 
+import com.yosto.yostobackend.gebruiker.Rol;
+
 public final class AuthenticationResponseBuilder {
-  String token;
+    String token;
+    Rol rol;
 
-  public AuthenticationResponseBuilder() {}
+    public AuthenticationResponseBuilder() {
+    }
 
-  public static AuthenticationResponseBuilder authenticationResponseBuilder() {
-    return new AuthenticationResponseBuilder();
-  }
+    public static AuthenticationResponseBuilder authenticationResponseBuilder() {
+        return new AuthenticationResponseBuilder();
+    }
 
-  public AuthenticationResponseBuilder setToken(String token) {
-    this.token = token;
-    return this;
-  }
+    public AuthenticationResponseBuilder setToken(String token) {
+        this.token = token;
+        return this;
+    }
 
-  public AuthenticationResponse build() {
-    return new AuthenticationResponse(this);
-  }
+    public AuthenticationResponseBuilder setRol(Rol rol) {
+        this.rol = rol;
+        return this;
+    }
+
+    public AuthenticationResponse build() {
+        return new AuthenticationResponse(this);
+    }
 }
